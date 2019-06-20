c2321acd017e1fbbe3a967b03fb9d6c296256ae3
Columbia91 <alpha.z91@mail.ru>
2019-06-20 20:03:56 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            Console.WriteLine("Host name: " + hostName);
+            IPHostEntry ipList = Dns.GetHostEntry(hostName);
+                //Dns.GetHostByName(hostName);
+
+            foreach (var ip in ipList.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            Console.WriteLine("Введите имя хоста: ");
+
+            // получение ай пи адресов по имени хоста
+            hostName = Console.ReadLine();
+            ipList = Dns.GetHostEntry(hostName);
+            
+            foreach (var ip in ipList.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            // получение по имени хоста ip адресов
+            var ipList2 = Dns.GetHostEntry(ipList.AddressList[0]);
+            foreach (var ip in ipList2.Aliases)
+            {
+                Console.WriteLine(ip);
+            }
+            Console.ReadLine();
+        }
+    }
+}
